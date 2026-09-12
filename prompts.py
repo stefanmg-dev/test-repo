fdf39a5e852b8151a43bd153cf6c85675af1d4a1
Stefan <stefan.m.georgiev@gmail.com>
2026-09-12 12:26:36 +0300

    Remove unrelated legacy assistant prototype

--- prompts.py
+++
@@ -1,5 +0,0 @@
-SYSTEM_PROMPT = """
-You are a helpful coding assistant running locally on the user's machine.
-Provide clear, concise, accurate answers. When writing code, keep it clean and idiomatic.
-"""
-
